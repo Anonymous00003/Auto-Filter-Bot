b9679c2338500b2394b93697c7c6c7b025dd7372
Anonymous00003 <128913792+Anonymous00003@users.noreply.github.com>
2025-01-26 20:05:09 +0530

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,9 @@
+movies = [
+    {"name": "Movie 1", "year": 2023, "genre": "Action", "rating": 8.2, "language": "English"},
+    {"name": "Movie 2", "year": 2022, "genre": "Comedy", "rating": 7.8, "language": "Spanish"},
+    {"name": "Movie 3", "year": 2021, "genre": "Drama", "rating": 7.9, "language": "French"},
+    {"name": "Movie 4", "year": 2023, "genre": "Action", "rating": 8.0, "language": "English"},
+    {"name": "Movie 5", "year": 2023, "genre": "Drama", "rating": 6.5, "language": "Italian"},
+    {"name": "Movie 6", "year": 2023, "genre": "Action", "rating": 8.7, "language": "English"},
+    {"name": "Movie 7", "year": 2024, "genre": "Action", "rating": 8.5, "language": "English"},  # Example new movie
+]
